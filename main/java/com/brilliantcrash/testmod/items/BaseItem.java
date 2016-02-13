74a8dc3bb6275a122933db195e8460a66df52530
BrilliantCrash <noahbeanjohnson@gmail.com>
2016-02-13 12:10:04 -0600

    Added recipes

---
+++ main/java/com/brilliantcrash/testmod/items/BaseItem.java
@@ -0,0 +1,21 @@
+package com.brilliantcrash.testmod.items;
+
+import com.brilliantcrash.testmod.ModCreativeTabs;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+/**
+ * Created by Noah on 2/13/2016.
+ */
+public class BaseItem extends Item {
+    public BaseItem (String unlocalizedName, CreativeTabs tab) {
+        super();
+
+        this.setUnlocalizedName(unlocalizedName);
+        this.setCreativeTab(tab);
+    }
+
+    public BaseItem (String unlocalizedName) {
+        this(unlocalizedName, ModCreativeTabs.testModItems);
+    }
+}
