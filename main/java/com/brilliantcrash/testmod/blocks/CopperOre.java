750b30dfb41c70f52ccfa3f6208897bb77710de4
BrilliantCrash <noahbeanjohnson@gmail.com>
2016-02-13 14:31:12 -0600

    Added new blocks: Copper Ore and Lead Ore, each with separate world generation. Added ingots to accompany them. Added full English localization to mod. (Most likely) finalized recipes for Glock 18 and 9mm ammunition.

---
+++ main/java/com/brilliantcrash/testmod/blocks/CopperOre.java
@@ -0,0 +1,14 @@
+package com.brilliantcrash.testmod.blocks;
+
+import net.minecraft.block.Block;
+
+/**
+ * Created by Noah on 2/13/2016.
+ */
+public class CopperOre extends BaseBlock {
+
+    public CopperOre(String unlocalizedName) {
+        super (unlocalizedName, 2.0f, 10.0f);
+    }
+
+}
---
+++ main/java/com/brilliantcrash/testmod/blocks/LeadOre.java
@@ -0,0 +1,12 @@
+package com.brilliantcrash.testmod.blocks;
+
+/**
+ * Created by Noah on 2/13/2016.
+ */
+public class LeadOre extends BaseBlock {
+
+    public LeadOre(String unlocalizedName) {
+        super (unlocalizedName, 2.0f, 10.0f);
+    }
+
+}
