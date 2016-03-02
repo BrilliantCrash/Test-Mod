6fafe978b2dae772d177415e8e03167d66d149ea
BrilliantCrash <noahbeanjohnson@gmail.com>
2016-03-02 17:13:01 -0600

    Removing traces of skeletons

--- main/java/com/brilliantcrash/tacticraft/items/ModGuns.java
+++ main/java/com/brilliantcrash/tacticraft/items/ModGuns.java
@@ -1,7 +1,6 @@
 package com.brilliantcrash.tacticraft.items;
 
 import com.brilliantcrash.tacticraft.items.guns.GunGlock;
-import com.brilliantcrash.tacticraft.items.guns.GunM4;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -11,11 +10,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModGuns {
 
     public static Item gunGlock;
-    public static Item gunM4;
 
     public static void createItems() {
         GameRegistry.registerItem(gunGlock = new GunGlock("gun_glock", 20, ModItems.ammo_ninemm, 40), "gun_glock");
-        //GameRegistry.registerItem(gunP250 = new GunGlock("gun_p250", 17), "gun_p250");
     }
 
 
